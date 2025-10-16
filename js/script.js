c357e3725b39b39f568c9dac97ee7bd54b87c932
Tomasz Kubiak <tomaszkubiak54@gmail.com>
2025-10-16 12:41:40 +0200

    function updateResultText add

--- js/script.js
+++ js/script.js
@@ -16,21 +16,26 @@
 		}
 	};
 
+	const updateResultText = (amount, result, currency) => {
+		const resultElement = document.querySelector(".js-result");
+
+		resultElement.innerHTML = `${amount.toFixed(
+			2
+		)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+	};
+
 	const onFormSubmit = (event) => {
 		event.preventDefault();
 
 		const amountElement = document.querySelector(".js-amount");
 		const currencyElement = document.querySelector(".js-currency");
-		const resultElement = document.querySelector(".js-result");
 
 		const amount = +amountElement.value;
 		const currency = currencyElement.value;
 
 		const result = calculateResult(amount, currency);
 
-		resultElement.innerHTML = `${amount.toFixed(
-			2
-		)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+		updateResultText(amount, result, currency);
 	};
 
 	const init = () => {
